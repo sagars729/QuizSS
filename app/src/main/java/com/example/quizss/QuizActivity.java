881043510f7c51e5373467fab6facea5f46757e5
sagars729 <sagardsaxena@gmail.com>
2019-02-13 23:39:21 -0500

    Scoring Implemented

--- app/src/main/java/com/example/quizss/QuizActivity.java
+++ app/src/main/java/com/example/quizss/QuizActivity.java
@@ -13,22 +13,30 @@ public class QuizActivity extends AppCompatActivity {
     private Button mFalseButton;
     private Button mSkipButton;
     private TextView mQuestionTextView;
+    private TextView mQuestionHeader;
+    private TextView mPointsTextView;
     private Question [] mQuestionBank = new Question[20];
 
     private int mCurrentIndex = -1;
+    private int mScore = 0;
     private boolean mLock = false;
 
     private void updateQuestion() {
         if(mCurrentIndex < mQuestionBank.length - 1) {
             mCurrentIndex++;
             mQuestionTextView.setText(mQuestionBank[mCurrentIndex].getTextResId());
+            mQuestionHeader.setText("Question " + mCurrentIndex);
+            mPointsTextView.setText("Points: " + mScore);
         }else mLock = true;
     }
 
     private void checkAnswer(boolean userPressedTrue){
         boolean answerIsTrue = mQuestionBank[mCurrentIndex].isAnswerTrue();
         int messageResId = R.string.incorrect_toast;
-        if(userPressedTrue == answerIsTrue) messageResId = R.string.correct_toast;
+        if(userPressedTrue == answerIsTrue){
+            messageResId = R.string.correct_toast;
+            mScore++;
+        }else mScore--;
         Toast.makeText(QuizActivity.this, messageResId, Toast.LENGTH_SHORT).show();
     }
 
@@ -37,16 +45,18 @@ public class QuizActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_quiz);
 
-
         mTrueButton = (Button)findViewById(R.id.true_button);
         mFalseButton = (Button)findViewById(R.id.false_button);
         mSkipButton = (Button) findViewById(R.id.skip_button);
         mQuestionTextView = (TextView) findViewById(R.id.question_text_view);
-        for(int i = 0; i < mQuestionBank.length; i+=1){
-            String qid = "q" + i;
-            String aid = "a" + i;
-            mQuestionBank[i] = new Question(getResources().getIdentifier(qid,"string",getPackageName()),getResources().getBoolean(getResources().getIdentifier(aid,"bool",getPackageName())));
-        }
+        mQuestionHeader = (TextView) findViewById(R.id.question_header);
+        mPointsTextView = (TextView) findViewById(R.id.points_text_view);
+        for(int i = 0; i < mQuestionBank.length; i+=1)
+            mQuestionBank[i] = new Question(
+                    getResources().getIdentifier("q" + i,"string",getPackageName()),
+                    getResources().getBoolean(getResources().getIdentifier("a" + i,"bool",getPackageName()))
+            );
+
         updateQuestion();
 
         mTrueButton.setOnClickListener(new View.OnClickListener() {
